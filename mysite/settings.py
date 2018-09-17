f761f638afca294b8f83b230e6ff2ec1707db8de
test <onelove2222@gmail.com>
2018-09-17 13:53:31 +0600

    Debug true declared.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ozmhndmq+a1^s_w89$4_kp&qenk)=-x^@%4+!vfe(^q$r#5f!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
